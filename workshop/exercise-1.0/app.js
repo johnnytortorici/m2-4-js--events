784e9278100f0697e5527ed81f36c51b98cd7cc3
johnnytortorici <johnny.tortorici@gmail.com>
2020-06-18 15:25:31 -0400

    Exercise 1.0

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,12 @@
 // Exercise 1.0
 // ------------
 console.log('exercise-1');
+
+const main = document.querySelector('#main');
+
+const clickScreen = function () {
+    main.innerHTML = '<h1>You win!</h1>';
+    main.removeEventListener('click', clickScreen);
+};
+
+main.addEventListener('click', clickScreen);
